997955260ec4e827bd29f6497262c7adf48b60fd
Shreya Bansal <67229689+Shreya935@users.noreply.github.com>
2021-06-08 23:22:08 +0530

    Create shivamcsc.cpp

---
+++ daapara/ambigu/shivamcsc.cpp
@@ -0,0 +1,20 @@
+saldfhsdfhsadfvsd
+af
+dsfsd
+f
+hsda
+fsdhf
+sadf
+sadhf<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$databasename="web";
+$conn = mysqli_connect($hostname, $username, $password, $databasename);
+if($conn) {
+    echo "Connection Successful...";
+}
+else {
+    echo "Connection failed.." + mysqli_connect_error();
+}
+?>
